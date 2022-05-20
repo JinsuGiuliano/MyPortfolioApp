f7e9ed1033338e5b0211a4d03f46776d70936491
Gustavo 친수 Giuliano <61397269+JinsuGiuliano@users.noreply.github.com>
2022-05-20 13:22:55 +0200

    erase send message

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -13,48 +13,6 @@ const Contact = () => (
             <div id="contact" className="box-shadow-full">
             <div className="row">
                 <div className="col-md-6">
-                <div className="title-box-2">
-                    <h5 className="title-left">
-                    Send Message Us
-                    </h5>
-                </div>
-                <div>
-                    <form className="contactForm">
-                    <div id="sendmessage">Your message has been sent. Thank you!</div>
-                    <div id="errormessage" />
-                    <div className="row">
-                        <div className="col-md-12 mb-3">
-                        <div className="form-group">
-                            <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                            <div className="validation" />
-                        </div>
-                        </div>
-                        <div className="col-md-12 mb-3">
-                        <div className="form-group">
-                            <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                            <div className="validation" />
-                        </div>
-                        </div>
-                        <div className="col-md-12 mb-3">
-                        <div className="form-group">
-                            <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                            <div className="validation" />
-                        </div>
-                        </div>
-                        <div className="col-md-12 mb-3">
-                        <div className="form-group">
-                            <textarea className="form-control" name="message" rows={5} data-rule="required" data-msg="Please write something for us" placeholder="Message" defaultValue={""} />
-                            <div className="validation" />
-                        </div>
-                        </div>
-                        <div className="col-md-12">
-                        <button type="submit" className="button button-a button-big button-rouded">Send Message</button>
-                        </div>
-                    </div>
-                    </form>
-                </div>
-                </div>
-                <div className="col-md-6">
                 <div className="title-box-2 pt-4 pt-md-0">
                     <h5 className="title-left">
                     Get in Touch
