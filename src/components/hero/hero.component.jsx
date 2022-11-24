db870ceb1595a4bba64a17e1440d698387232aa0
Gustavo Giuliano <61397269+JinsuGiuliano@users.noreply.github.com>
2022-11-24 09:39:40 -0300

    EDIT: hero key words

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
-
+import React from "react";
 
 const Hero = () => (
-
-    <div id="home" className="intro route bg-image" style={{backgroundImage: 'url(img/intro-bg.jpg)'}}>
-        <div className="overlay-itro" />
-            <div className="intro-content display-table">
-                <div className="table-cell">
-                    <div className="container">
-                
-                    <h1 className="intro-title mb-4">I am Gustavo Giuliano</h1>
-                    <p className="intro-subtitle"><span className="text-slider-items">Web Developer,Web Designer,MERN Stack,React Js,Firestore Cloud,Node Js,Express Js </span><strong className="text-slider" /></p>
-                
-                    </div>
-                </div>
-        </div>
-    </div>
-)
+	<div
+		id="home"
+		className="intro route bg-image"
+		style={{ backgroundImage: "url(img/intro-bg.jpg)" }}
+	>
+		<div className="overlay-itro" />
+		<div className="intro-content display-table">
+			<div className="table-cell">
+				<div className="container">
+					<h1 className="intro-title mb-4">I am Gustavo Giuliano</h1>
+					<p className="intro-subtitle">
+						<span className="text-slider-items">
+							ReactJS,Svelte,Golang,Firestore,NodeJS,ExpressJs{" "}
+						</span>
+						<strong className="text-slider" />
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+);
 
 export default Hero;
