737fac75d608dc07e83bcdf2f7998f65fde11485
Gustavo Giuliano 친수 <jinsu.gustavo.giuliano@gmail.com>
2024-03-28 23:21:55 -0400

    update about

--- src/components/services/services.component.jsx
+++ src/components/services/services.component.jsx
@@ -9,31 +9,14 @@ const Services = () => (
       <div className="col-sm-12">
         <div className="title-box text-center">
           <h3 className="title-a">
-            Services
+            How can I help you?
           </h3>
           <p className="subtitle-a">
-            These are the main areas I've been working the last three years
-          </p>
+          Ready to embark on a journey of innovation and success? Let's collaborate to bring your vision to life.
+                Contact me today to discuss how I can contribute to your projects and help you achieve your goals.          </p>
           <div className="line-mf" />
         </div>
       </div>
-    </div>
-    <div className="row">
-      <div className="col-md-6">
-        <div className="service-box">
-          <div className="service-ico">
-            <span className="ico-circle"><i className="ion-code-working" /></span>
-          </div>
-          <div className="service-content">
-            <h2 className="s-title">Web Development</h2>
-            <p className="s-description text-center">
-                Ready to embark on a journey of innovation and success? Let's collaborate to bring your vision to life.
-                Contact me today to discuss how I can contribute to your projects and help you achieve your goals.
-            </p>
-          </div>
-        </div>
-      </div>
-    
     </div>
   </div>
 </section>
